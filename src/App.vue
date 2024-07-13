2243d9124a927d933eb8da53902b1b0acf227af7
Lorens Osman <lorens.osman.dev@gmail.com>
2024-07-13 19:45:24 +0300

    starter

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Child from "./components/child.vue";
+</script>
+
+<template>
+  
+   <h1>hiiiiiiii</h1>
+  <Child/>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped lang="less">
+
+</style>
---
+++ src/components/child.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h3>from child</h3>
+   
+
+
+</template>
+
+<style scoped lang="less">
+
+</style>
